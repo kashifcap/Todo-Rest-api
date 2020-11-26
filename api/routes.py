9e85443e5d5361ad80dfed2e79c97f795913a468
kashif <firozkashif1234@gmail.com>
2020-11-26 15:46:37 +0530

    added login route and added jwt

--- api/routes.py
+++ api/routes.py
@@ -3,8 +3,36 @@ from api import app,db
 from api.models import User,Todo
 from api.schema import user_schema, todo_schema, todos_schema
 import uuid
+import jwt
+import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
 
+
+def token_required(f):
+    @wraps(f)
+    def decorated(*args,**kwargs):
+        token = None
+
+        if not 'x-access-token' in request.headers:
+            token = request.headers['x-access-token']
+        
+        if not token:
+            return jsonify({'msg' : 'Invalid Token!'}),401
+        
+        try:
+            data = jwt.decode(token, app.config['SECRET_KEY'])
+            current_user = User.query.filter_by(public_id = data['public_id']).first()
+        except:
+            retrun jsonify({'msg' : 'Invalid Token!'}), 401
+        
+        if not current_user:
+            return jsonify({'msg' : 'Invalid Token'}), 401
+        
+        return f(current_user, *args,**kwargs)
+
+
+
+    return decorated
 
 
 @app.route('/register', methods = ['POST'])
@@ -27,4 +55,27 @@ def register():
     return_schema = user_schema.dump(user)
 
     return jsonify({'message': 'User added successfully', 'user': return_schema})
+
+
+@app.route('/login')
+def login():
+    data = request.authorization
+
+    if not data or not data.username or not data.password:
+        return jsonify({'msg' : 'Invalid Credentials!'}), 401
     
+    user = User.query.filter_by(username=data.username).first()
+
+    if not user:
+        return jsonify({'msg' : 'Invalid Credentials!'}), 401
+    
+    if check_password_hash(user.password, data.password):
+        token = jwt.encode({'public_id': user.public_id, 'exp': datetime.datetime.utcnow(
+        ) + datetime.timedelta(minutes=30)}, app.config['SECRET_KEY'])
+        return jsonify({'msg' : 'Login success', 'x-access-token' : token.decode('UTF-8')})
+    
+    return jsonify({'msg' : 'Invalid Credentials!'}), 401
+
+
+@app.route('/todo', methods=['GET'])
+def get_todo():
